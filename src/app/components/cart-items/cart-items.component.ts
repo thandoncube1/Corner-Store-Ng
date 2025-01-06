60eaf99b6ce10072d536e26c57a3bcde382ba317
thandoncube1 <ncubethando20@gmail.com>
2025-01-06 01:31:13 -0500

    Working on the products application - Corner Store.

---
+++ src/app/components/cart-items/cart-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-items',
+  imports: [],
+  templateUrl: './cart-items.component.html',
+  styleUrl: './cart-items.component.css'
+})
+export class CartItemsComponent {
+
+}
