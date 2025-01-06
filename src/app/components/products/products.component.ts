270c5f9c45b539024987a38b7354cc16cc4ec69f
thandoncube1 <ncubethando20@gmail.com>
2025-01-06 02:52:41 -0500

    Angular application - current CORS bugs.

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { IProduct } from '../../model/interface/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  http = inject(HttpClient);
+  productList: IProduct[] = [];
+
+
+  // Life cycle method for the NG
+  ngOnInit(): void {
+    this.getAllProducts();
+    console.log(environment.API_URL + "GetAllProducts", { "Access-Control-Allow-Origin": "*" });
+  }
+
+  getAllProducts() {
+    this.http.get(environment.API_URL + "GetAllProducts").subscribe((res: any) => {
+       this.productList = res.data;
+    });
+  }
 
 }
